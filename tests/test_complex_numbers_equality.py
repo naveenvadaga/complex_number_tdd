23f1154a174f39ee61530ea343741e318ae6cebc
naveenvadaga <naveenvadaga@gmail.com>
2019-08-01 10:08:43 +0530

    test object initialization 1

---
+++ tests/test_complex_numbers_equality.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class TestComplexNumbersEquality(unittest.TestCase):
+    def test_complex_numbers_initialization(self):
+        real_part = 1
+        imaginary_part = 10
+        assert ComplexNumber(real_part, imaginary_part).real == real_part
