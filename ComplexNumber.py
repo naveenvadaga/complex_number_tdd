22003cc587dc37c2af12a55270bfcaa2daf448a5
naveenvadaga <naveenvadaga@gmail.com>
2019-08-02 15:16:25 +0530

    test complex  division 24

--- ComplexNumber.py
+++ ComplexNumber.py
@@ -23,12 +23,7 @@ class ComplexNumber:
         return ComplexNumber(real_part, imaginary_part)
 
     def conjugate(self):
-        if self.imaginary == 0:
-            return ComplexNumber(self.real, 0)
-        elif self.real == 0:
-            return ComplexNumber(0, - self.imaginary)
-        else:
-            return ComplexNumber(self.real, -self.imaginary)
+        return ComplexNumber(self.real, -self.imaginary)
 
     def __truediv__(self, other):
         if self.imaginary == 0 and other.imaginary == 0:
